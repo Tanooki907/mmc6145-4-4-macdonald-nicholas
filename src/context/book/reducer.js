c34232eec1e58d798deb134c4b24be0dc404bbf1
Nicholas MacDonald <macdonaldn@ufl.edu>
2023-09-26 21:56:59 -0400

    context? reducer??

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,5 +1,22 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+import {
+  ADD_BOOK,
+  REMOVE_BOOK,
+  SEARCH_BOOKS
+} from './actions'
+
+export default function reducer(prevState, {action, payload}) {
+  switch(action) {
+    case ADD_BOOK:
+      return saveToLocalStorage([...favoriteBooks, payload])
+    case REMOVE_BOOK:
+      return saveToLocalStorage(favoriteBooks.filter(favBook => favBook !== payload))
+    case SEARCH_BOOKS:
+      return 
+    default:
+      return prevState
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
--- src/pages/book/index.jsx
+++ src/pages/book/index.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams, useNavigate, Navigate } from "react-router-dom"
 import { useBookContext } from "../../context/book"
 import styles from "./style.module.css"
+import { ADD_BOOK, REMOVE_BOOK } from "../../context/book/actions"
 // TODO: import actions
 
 export default function Book() {
@@ -24,11 +25,11 @@ export default function Book() {
         {
           isFavoriteBook
           // TODO: add onClick function that calls dispatch to remove a book
-          ? <button>
+          ? <button onClick={() => dispatch({action: REMOVE_BOOK})}>
               Remove from Favorites
             </button>
           // TODO: add onClick function that calls dispatch to add a book
-          : <button>
+          : <button onClick={() => dispatch({action: ADD_BOOK})}>
               Add to Favorites
             </button>
         }
